d6bd20bcb5baa47ef93b12e09d2fefa398119d7d
sml93 <shawnmlee93@gmail.com>
2022-05-17 16:21:55 +0800

    First Commit

---
+++ network.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import torch
+import numpy as np
+import torch.nn.functional as F
+
+from torch import nn
+
+class FeedForwardNN(nn.Module):
+  def __init__(self, in_dim, out_dim):
+      super(FeedForwardNN, self).__init__()
+
+      self.layer1 = nn.Linear(in_dim, 64)
+      self.layer2 = nn.Linear(64, 64)
+      self.layer3 = nn.Linear(64, out_dim)
+  
+  def forward(self, obs):
+    # Convert observation to tensor if it's a numpy array
+    if isinstance(obs, np.ndarray):
+      obs = torch.tensor(obs, dtype=torch.float)
+    activation1 = F.relu(self.layer1(obs))
+    activation2 = F.relu(self.later2(activation1))
+    output = self.layer3(activation2)
+    return output
